8443926168e2e88acbc27dfe2f64155b103243f7
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-11-04 13:26:38 +0300

    Реализован метод наискорейшего градиентного спуска

--- LaboratoryWork2/Program.cs
+++ LaboratoryWork2/Program.cs
@@ -1,5 +1,22 @@
 using LaboratoryWork2.Tasks.Task1;
+using LaboratoryWork2.Tasks.Task2;
 
-var solver = new Task1Solver();
-var result = solver.Solve();
-solver.PrintResult(result);
+Console.WriteLine("Выберите задачу:");
+Console.WriteLine("1 - Метод градиентного спуска с постоянным шагом");
+Console.WriteLine("2 - Метод наискорейшего градиентного спуска");
+Console.Write("Введите номер (1 или 2): ");
+
+string? choice = Console.ReadLine();
+
+if (choice == "2")
+{
+    var solver2 = new Task2Solver();
+    var result2 = solver2.Solve();
+    solver2.PrintResult(result2);
+}
+else
+{
+    var solver1 = new Task1Solver();
+    var result1 = solver1.Solve();
+    solver1.PrintResult(result1);
+}
